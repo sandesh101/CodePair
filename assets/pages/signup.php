177eae986c1840c07a8f3fc3a0c118795bc3a90d
Sandesh Rimal <rimal.sandesh11@gmail.com>
2022-03-26 11:13:26 +0545

    Style Sign Up Page

--- assets/pages/signup.php
+++ assets/pages/signup.php
@@ -1,31 +1,34 @@
-
+<?php
+    $title = "Sign Up";
+    include('header.php');
+?>
 
 <div class="container">
     <div class="form_field">
         <h1>Sign Up</h1>
         <form action="" method="post">
             <div class="fname">
-                <label for="fname">Full name:</label>
+                <label for="fname">Full name:</label><br>
                 <input type="text" name="fname" placeholder="Full Name">
             </div>
             <div class="email">
-                <label for="email">Email:</label>
+                <label for="email">Email:</label><br>
                 <input type="email" name="email" placeholder="Email">
             </div>
             <div class="username">
-                <label for="username">Username:</label>
+                <label for="username">Username:</label><br>
                 <input type="text" name="username" placeholder="Username">
             </div>
             <div class="password">
-                <label for="password">Password:</label>
+                <label for="password">Password:</label><br>
                 <input type="password" name="password" placeholder="Password">
             </div>
             <div class="language">
-                <label for="language">Language:</label>
+                <label for="language">Language:</label><br>
                 <input type="text" name="language" placeholder="Language to learn">
             </div>
             <div class="button">
-                <input type="submit" value="Register">
+                <input type="submit" value="Register" id="button">
             </div>
         </form>
     </div>
