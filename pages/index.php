808f3fd913f9dd71493564c228f76ed9679f3d2f
Sandesh Rimal <rimal.sandesh11@gmail.com>
2022-03-26 13:53:24 +0545

    Changes in login page

--- pages/index.php
+++ pages/index.php
@@ -1,7 +1,7 @@
 <?php
     $title = "Home";
-    require_once('./header.php');
-    require_once('./footer.php');
+    // require_once('./header.php');
+    // require_once('./footer.php');
 ?>
 
 <a href="./signup.php">Sign Up</a>
