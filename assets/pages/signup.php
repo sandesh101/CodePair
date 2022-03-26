53335f61a1656a4c6e6cbe8350f8e1093ddb0e5c
Sandesh Rimal <rimal.sandesh11@gmail.com>
2022-03-26 10:30:29 +0545

    Made Sing Up Page

---
+++ assets/pages/signup.php
@@ -0,0 +1,32 @@
+
+
+<div class="container">
+    <div class="form_field">
+        <h1>Sign Up</h1>
+        <form action="" method="post">
+            <div class="fname">
+                <label for="fname">Full name:</label>
+                <input type="text" name="fname" placeholder="Full Name">
+            </div>
+            <div class="email">
+                <label for="email">Email:</label>
+                <input type="email" name="email" placeholder="Email">
+            </div>
+            <div class="username">
+                <label for="username">Username:</label>
+                <input type="text" name="username" placeholder="Username">
+            </div>
+            <div class="password">
+                <label for="password">Password:</label>
+                <input type="password" name="password" placeholder="Password">
+            </div>
+            <div class="language">
+                <label for="language">Language:</label>
+                <input type="text" name="language" placeholder="Language to learn">
+            </div>
+            <div class="button">
+                <input type="submit" value="Register">
+            </div>
+        </form>
+    </div>
+</div>
